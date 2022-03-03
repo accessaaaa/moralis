f24f86386d64a5fee871de51f0f9c275f355183a
accessaaaa <98718384+accessaaaa@users.noreply.github.com>
2022-03-03 14:12:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+/* Moralis init code */
+const serverUrl = "https://l478b20ch2lk.usemoralis.com:2053/server";
+const appId = "Ko2wwJZROceYA85jlSSzwAbJYYr3pSlWawzb3Yxp";
+Moralis.start({ serverUrl, appId });
+
+/* TODO:sCyg3vJ6zpjRmaSVbztU6ZiYRkEDPFPyE8HPQcZ2 */
